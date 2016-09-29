0a8cf0e8b63a76191786738958bd273049f825ae
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2016-09-29 17:05:46 -0400

    Trying to add a damn menu....nothing works...

--- app/src/main/java/com/example/jason/jasonpearson_comp304lab2_ex1/DisplayAmericanActivity.java
+++ app/src/main/java/com/example/jason/jasonpearson_comp304lab2_ex1/DisplayAmericanActivity.java
@@ -2,6 +2,12 @@ package com.example.jason.jasonpearson_comp304lab2_ex1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.view.menu.MenuView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.w3c.dom.Text;
 
 public class DisplayAmericanActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class DisplayAmericanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_american);
+    }
+
+    protected boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menuamerican, menu);
+        return true;
     }
 }
